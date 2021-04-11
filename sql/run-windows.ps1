a8f3259c96d9e00f91aaa4b01178502eceeac6c4
vtheil <64448057+vtheil@users.noreply.github.com>
2021-04-11 08:59:31 -0700

    Update run-windows.ps1

--- sql/run-windows.ps1
+++ sql/run-windows.ps1
@@ -12,13 +12,13 @@ echo "Waiting for database to initialize"
 sleep 5
 
 # Create Database
-& docker-compose exec db bash -c "psql -U postgres -c 'CREATE DATABASE moderesales'"
+& docker-compose exec db bash -c "psql -U postgres -c 'CREATE DATABASE example'"
 
 # Populate the new database
-& docker-compose exec db bash -c "psql -U postgres -d moderesales -f /var/lib/postgresql/modere/generate_database.sql"
+& docker-compose exec db bash -c "psql -U postgres -d example -f /var/lib/postgresql/example/generate_database.sql"
 
 # Export the new content into a csv file
-& docker-compose exec db bash -c "psql -U postgres -d moderesales -c '\copy (SELECT customers.id AS customer_id, customers.first_name, customers.last_name, products.id AS product_id, products.product_name, products.price FROM customers JOIN purchases ON customers.id = purchases.customer_id JOIN products ON products.id = purchases.product_id ORDER BY customer_id ASC) TO /var/lib/postgresql/modere/result.csv CSV HEADER'"
+& docker-compose exec db bash -c "psql -U postgres -d example -c '\copy (SELECT customers.id AS customer_id, customers.first_name, customers.last_name, products.id AS product_id, products.product_name, products.price FROM customers JOIN purchases ON customers.id = purchases.customer_id JOIN products ON products.id = purchases.product_id ORDER BY customer_id ASC) TO /var/lib/postgresql/modere/result.csv CSV HEADER'"
 
 # Shutdown the container
 & docker-compose down
